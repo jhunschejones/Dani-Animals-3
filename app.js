90c6bcdc4949f1240cdaf0272ca9166ef6229481
Joshua Hunsche Jones <joshua@hunschejones.com>
2018-08-21 07:55:15 -0700

    Add pm2, morgan, and watson for logging

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var newrelic = require('newrelic');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var requestLogger = require('./bin/requestLogger');
 
 // Connecting my database
 var mongo = require('mongodb');
@@ -27,7 +27,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
